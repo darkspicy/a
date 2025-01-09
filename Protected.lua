ea6dbab2759ce4a64934146c31647307a790a738
hasdhashdasdas <101713683+darkspicy@users.noreply.github.com>
2025-01-10 04:49:02 +0800

    Update Protected.lua

--- Protected.lua
+++ Protected.lua
@@ -1,6 +1,3 @@
-local a=loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/LuaU-Free-Key-System-UI/main/source.lua"))()local b=false;local a=a:Init({Debug=false,Title="NemsHubV1 Key System",Description=nil,Link="https://direct-link.net/1268518/fisch-script-update",Discord="https://discord.gg/ag6WNjudvA",SaveKey=nil,Verify=function(a)if a=="UPDATEV1"then b=true;return true else return false end end,GuiParent=game.CoreGui})
-
-
 
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Knuxy92/Ui-linoria/main/Fluent/Fluent.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
@@ -3593,11 +3590,11 @@ AllFuncs['Auto Find Boat Event'] = function()
 			continue
 		end
 		if Config["Farm Fish"] then
-			Config["Farm Fish"] = false
+			Config["Farm Fish"] = true
 			continue
 		end
 		if Config['To Pos Stand'] then
-			Config['To Pos Stand'] = false
+			Config['To Pos Stand'] = true
 			continue
 		end
 		for _, IngredientName in pairs(IngredientList) do
@@ -3653,8 +3650,8 @@ AllFuncs['Auto Find Boat Event'] = function()
 			end
 		end
 		if Config['Back To Fishing'] and not _hasItem("Witches Ingredient") and #IngredientList <= 0 then
-			Config["Farm Fish"] = false
-			Config['To Pos Stand'] = false
+			Config["Farm Fish"] = true
+			Config['To Pos Stand'] = true
 			task.spawn(AllFuncs['Farm Fish'])
 			task.spawn(AllFuncs['To Pos Stand'])
 		end
